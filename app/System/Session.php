a8899240fb69e847eb65192d1109e389ab958ea7
hondare <hondare@indival.co.jp>
2020-07-14 16:46:03 +0900

    Sessionクラス（修正必要）

---
+++ app/System/Session.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\System;
+
+use App\System\Interfaces\SessionInterface;
+
+class Session implements SessionInterface
+{
+//FIXME（PHPの本写した）
+    protected static function $sessionStarted = false;
+    protected static function $sessionIdRegenerated = false;
+
+    public  function __construct()
+    {
+        if (!self::$sessionStarted){
+            session_start();
+
+            self::$sessionStarted = true;
+        }
+
+    }
+
+
+    public function set(string $name, string $value)
+    {
+        $_SESSION[$name] = $value;
+    }
+
+    function get(string $name, $default = null)
+    {
+        if (isset($_SESSION[$name])){
+            return $_SESSION[$name];
+        }
+        return  $default;
+
+    }
+
+    function remove(string $name)
+    {
+        unset($_SESSION[$name]);
+
+    }
+
+//clearメソッド→$_SESSIONを空にする
+
+//:voidはPHP7以上。この型宣言は型を返すとエラーになる
+//FIXME（:voidでええんか？）
+    public function clear():void
+    {
+        $_SESSION = array();
+    }
+
+//FIXME（:voidでええんか？）
+    function regenerate($destroy = true):void
+    {
+        // TODO: Implement regenerate() method.
+        if (!self::$sessionIdRegenerated){
+        session_regenerate_id($destroy);
+
+        self::$sessionIdRegenerated = true;
+        }
+    }
+
+    function setAuthenticated($bool)
+    {
+        // TODO: Implement setAuthenticated() method.
+        $this->set('_authenticated',(bool)$bool);
+
+        $this->regenerate();
+    }
+
+    function isAuthenticated()
+    {
+        return $this->get('_authenticated',false);
+    }
+}
