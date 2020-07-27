deb8ecddb4ee53941a7eaeaa4d9bafb0711edf56
ebinase <ebinase@indival.co.jp>
2020-07-27 13:01:25 +0900

    ツイートテーブルのマイグレーション作成

---
+++ resources/views/layouts/layout.php
@@ -0,0 +1,12 @@
+<html lang="ja">
+<head>
+    <title>tweetworks</title>
+</head>
+<body>
+    <header style="background-color: #cecece">ここはヘッダーです</header>
+    <div class="content">
+        <?= $_content ?>
+    </div>
+    <footer style="background-color: #cecece">ここはフッターです。</footer>
+</body>
+</html>
