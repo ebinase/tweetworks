e0854706a0a5b5327551195364b6b86af7d92efe
ebinase <ebinase@indival.co.jp>
2020-07-30 13:12:14 +0900

    Viewファイルの修正

--- app/System/View.php
+++ app/System/View.php
@@ -16,12 +16,15 @@ class View
 
     public function setLayoutVar($name, $value)
     {
+        $value = $this->_escapeVariables($value);
         $this->_layout_variables[$name] = $value;
     }
 
+    //変数展開を行うため、変数名の重複がないように引数にはアンダースコアをつける
     public function render($_path, $_variables =[], $_layout_path = false)
     {
         $_file = $this->_base_dir . '/' . $_path . '.php';
+
 
         //変数展開
         extract(array_merge($this->_defaults, $_variables));
@@ -54,6 +57,30 @@ class View
         }
 
         return $content;
+    }
+
+    //renderメソッド内で使うと_contentの値もエスケープされるので注意！！
+    /**
+     * 配列(多次元やネストしたものを含む)のすべての要素をエスケープ
+     *
+     * @param array | string $variables
+     * @return mixed
+     */
+    private function _escapeVariables($variables)
+    {
+        if (is_array($variables)) {
+            foreach ($variables as $key => $value) {
+                if (is_array($value)) {
+                    $variables[$key] = $this->_escapeVariables($value);
+                }else {
+                    $variables[$key] = $this->escape($value);
+                }
+            }
+        } else {
+            $variables = $this->escape($variables);
+        }
+
+        return $variables;
     }
 
     public function escape($string)
