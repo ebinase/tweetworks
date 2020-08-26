98376b0df94f32a4a5b033bffcfe74886dbf937b
ebinase <ebinase@indival.co.jp>
2020-08-26 20:03:53 +0900

    お気に入り機能のバックエンド側実装

--- app/Controller/FavoriteController.php
+++ app/Controller/FavoriteController.php
@@ -2,14 +2,51 @@
 
 namespace App\Controller;
 
+use App\Model\Favorite;
 use App\System\Classes\Facades\Auth;
-use App\System\Classes\Services\Service;
+use App\System\Classes\HTTP\Response;
+use App\System\Interfaces\HTTP\RequestInterface;
 
 class FavoriteController extends \App\System\Classes\Controller
 {
-    public function update()
+    public function update(RequestInterface $request)
     {
+        //ログイン中のユーザーid
         $user_id = Auth::id();
 
+        //ajax(post)で送られてきたツイートid
+        $tweet_id = $request->getPost('tweet_id');
+
+
+        $favorite = new Favorite();
+        //すでにお気に入りしてるかチェック
+        $count = $favorite->checkIfFavoriteTweet($user_id, $tweet_id);
+
+        if ($count == 0) {
+            $favorite->smartInsert([
+                'user_id' => $user_id,
+                'tweet_id' => $tweet_id,
+            ]);
+            $result = 'set';
+        } else {
+            $favorite->unsetUsersFav($user_id, $tweet_id);
+            $result = 'unset';
+        }
+//
+        $favs = $favorite->countFavs($tweet_id);
+
+        $content = [
+            'result' => $result,
+            'favs' => $favs,
+        ];
+
+        $response = new Response();
+
+        $response->setContent(json_encode($content));
+
+        //ajax側でdataType:Jsonとしているため不要だが、明示的に記述
+        $response->setHttpHeader('Content-type', 'application/json; charset=UTF-8');
+
+        return $response;
     }
 }
