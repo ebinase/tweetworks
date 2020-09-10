1907d3ad35c89e0d8493bdcaa028c4f749076c20
ebinase <ebinase@indival.co.jp>
2020-09-10 12:26:23 +0900

    phpinfo表示

--- public/index.php
+++ public/index.php
@@ -5,20 +5,22 @@ use App\System\Classes\Core\Application;
 
 require_once '../vendor/autoload.php';
 
-//Application呼び出し
-$application = new Application();
-$request = $application->getRequest();
+phpinfo();
 
-//ミドルウェアのリストを作成
-$kernel = new Kernel($request);
-//コントローラ呼び出しハンドラとミドルウェアたちをインスタンス化して一連のパイプラインを作成
-$pipeline = $kernel->build();   //内部でnewするため依存性高め
-
-//パイプラインを元にミドルウェアとハンドラを実行してレスポンスを生成。
-$response = $kernel->handle($request, $pipeline);
-
-//レスポンスをセット
-$application->setResponse($response);
-
-//送信
-$application->send();
+////Application呼び出し
+//$application = new Application();
+//$request = $application->getRequest();
+//
+////ミドルウェアのリストを作成
+//$kernel = new Kernel($request);
+////コントローラ呼び出しハンドラとミドルウェアたちをインスタンス化して一連のパイプラインを作成
+//$pipeline = $kernel->build();   //内部でnewするため依存性高め
+//
+////パイプラインを元にミドルウェアとハンドラを実行してレスポンスを生成。
+//$response = $kernel->handle($request, $pipeline);
+//
+////レスポンスをセット
+//$application->setResponse($response);
+//
+////送信
+//$application->send();
