d2999324a7bd7d1066248cb9a43efced091a6845
ebinase <ebinase@indival.co.jp>
2020-09-23 10:27:31 +0900

    マイグレーションやシーディングの修正

--- app/Controller/Admin/DatabaseController.php
+++ app/Controller/Admin/DatabaseController.php
@@ -14,51 +14,35 @@ class DatabaseController extends Controller
         return $this->render('admin/database');
     }
 
-    public function migrate(RequestInterface $request)
+    public function migrate()
     {
-        $this->authenticate($request);
-
         $migration = new \Database\migration\Migration();
         $migration->up();
 
         return "migration完了";
     }
 
-    public function refresh(RequestInterface $request)
+    public function refresh()
     {
-        $this->authenticate($request);
-
         $migration = new \Database\migration\Migration();
         $migration->refresh();
 
         return "refresh完了";
     }
 
-    public function seed(RequestInterface $request)
+    public function seed()
     {
-        $this->authenticate($request);
-
         $seed = new \Database\seeds\Seeder();
         $seed->run();
 
         return "seed完了";
     }
 
-    public function refreshAndSeed(RequestInterface $request) {
+    public function refreshAndSeed() {
         print $this->refresh() . '<br>';
         print $this->seed() . '<br>';
 
         return 'refresh&seed完了<br><a href="'. url('/home') . '">ホームへ</a>';
     }
 
-    private function authenticate(RequestInterface $request)
-    {
-        $input_pass = $request->getPost('password', '');
-        // 入力されたパスワードが管理者パスワードと一致するかチェック
-        if ($input_pass = Env::get('ADMIN_PASSWORD')) {
-            return true;
-        }
-
-        throw new UnauthorizedException('実行権限がありません。');
-    }
 }
